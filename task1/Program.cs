cbe43fac89d9f67b08b2f7e0355a8f1f83f6bb84
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2022-04-17 10:49:58 +0300

    task 1 программа принимает на вход координаты и определяет к какой четверти принадлежит точка

---
+++ task1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User!");
+
+double x = 1;
+double y = -0.5;
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("число находится в 1 четверти");
+}
+if (x > 0 && y < 0)
+{
+    Console.WriteLine("число находится в 4 четверти");
+}
+if (x < 0 && y > 0)
+{
+    Console.WriteLine("число находится во 2 четверти");
+}
+if (x < 0 && y < 0)
+{
+    Console.WriteLine("число находится в 3 четверти");
+}
