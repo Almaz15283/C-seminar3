bd9b819352f35cf97c5746394fcdf9a7d8259679
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2022-04-17 11:16:55 +0300

    по номеру четверти программа показывает диапазон значений

---
+++ task2/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User!0");
+Console.WriteLine(" Введите номер четверти");
+string quarter = Console.ReadLine();
+int a = Convert.ToInt32(quarter);
+
+if (a == 1)
+{
+    Console.WriteLine(" Х любое положительное и У любое положительное");
+
+}
+if (a == 2)
+{
+    Console.WriteLine(" Х любое отрицательное и У любое положительное");
+
+}
+if (a == 3)
+{
+    Console.WriteLine(" Х любое отрицательное и У любое отрицательное");
+
+}
+if (a == 4)
+{
+    Console.WriteLine(" Х любое положительное и У любое отрицательное");
+
+}
