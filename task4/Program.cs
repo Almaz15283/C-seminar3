4ec32d8b71747ef0d3f372ceffdef3a2172fcce9
Almaz15283 <102859501+Almaz15283@users.noreply.github.com>
2022-04-17 11:59:38 +0300

    построить таблицу квадратов от 1 до N

---
+++ task4/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, User!");
+Console.WriteLine("Посчитать таблицу квадратов чисел от 1 до N ")
+int n = 5;
+int i = 1;
+int sqr = 0;
+while (i <= n)
+{
+    sqr = i*i;
+    Console.Write($"{sqr}; ", sqr); 
+    i++;
+}
+
+
+
+
+
+
+
+
